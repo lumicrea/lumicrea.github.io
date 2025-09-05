2f8f1b0e399dbd6e7aa765735c2669c8e037536f
Lumicrea <51113443+lumicrea@users.noreply.github.com>
2025-09-05 13:43:58 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -2,28 +2,28 @@ const projects = {
   1: {
     id: 1,
     title: "Project One",
-    image: "project1.jpg",
+    image: "project1.png",
     description: "This is a detailed description of Project One.",
     category: "Analytics"
   },
   2: {
     id: 2,
     title: "Project Two",
-    image: "project2.jpg",
+    image: "project2.png",
     description: "This is a detailed description of Project Two.",
     category: "Design"
   },
   3: {
     id: 3,
     title: "Project Three",
-    image: "project3.jpg",
+    image: "project1.png",
     description: "This is a detailed description of Project Three.",
     category: "Research"
   },
   4: {
     id: 4,
     title: "Project Four",
-    image: "project4.jpg",
+    image: "project2.png",
     description: "This is a detailed description of Project Four.",
     category: "Web Apps"
   }
